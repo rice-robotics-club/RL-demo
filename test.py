55d328e4cef3e52f5a4f7fb19190482b3a6c49c1
Clay Goldsmith <buzzscoutbark@gmail.com>
2025-09-22 13:44:40 -0500

    test.py quick description

---
+++ test.py
@@ -0,0 +1,6 @@
+'''
+Currently this file doesn't do anything but eventually we should have some sort of quick, headless 
+test framework written here so we can quickly run a bunch of tests on a model and see how it performs 
+in different scenarios/environments. -clay
+'''
+
